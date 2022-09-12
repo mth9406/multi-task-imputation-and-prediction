0712a22faf7ea6660b4f2bfddbdd94898e24318c
mth9406 <mth9406@naver.com>
2022-09-12 18:58:25 +0900

    developing GraphLearningLayer

--- main/layers/gnnLayers.py
+++ main/layers/gnnLayers.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn 
 from torch.nn import functional as F
-from torch_geometric.nn import MessagePassing
+import numpy as np
+# from torch_geometric.nn import MessagePassing
 
 from torch_scatter import scatter_add, scatter_mean, scatter_max, scatter_sum
 
@@ -41,6 +42,7 @@ class GCNBlock(nn.Module):
         r"""
         node_emb: node embedding (bs, node_emb_size)
         edge_emb: edge embedding (num_edges, edge_emb_size)
+        feature_emb: feature embedding (num_features, node_emb_size)
         edge_index: edge index (adj matrix in COO format) (2, num_edges)
         """
         src, dst = edge_index
@@ -96,3 +98,91 @@ class NodePredictionHead(nn.Module):
             return self.decode(x_imputed).flatten() 
         else: 
             return self.decode(x_imputed)
+
+def coo_to_adj(rel, num_objects, device= None):
+    r"""Converts COO-type to adjacency matrix
+    
+    # Arguments          
+    ___________ 
+    rel : torch.FloatTensor 
+        relation - tensor                  
+    num_objects : int
+        the number of objects               
+    device : torch.device     
+        device - cpu or cuda    
+
+    # Returns    
+    _________     
+    adj : torch.FloatTensor
+        adjacency matrix  
+        a square matrix of shape: num_object x num_object 
+    """
+
+    adj = torch.zeros((num_objects*num_objects))
+    for i in range(num_objects-1):
+        adj[i*num_objects+i+1:(i+1)*num_objects+(i+1)] = rel[i*num_objects:(i+1)*num_objects]
+    if device is not None:
+        adj = adj.to(device)
+    return adj.reshape((num_objects,num_objects))
+
+def gumbel_softmax(logits, tau, hard= True, dim= 1):
+    r"""
+    returns a continuous approximation of the discrete distribution 
+    # Arguments   
+    ___________       
+    logits: torch FloatTensor type 
+        logits    
+    tau: float 
+        softmax temperature - a parameter that controls the smoothness of the samples   
+    hard: bool 
+        hard sampling if set true 
+    """ 
+    return F.gumbel_softmax(logits, tau=tau, hard=hard, dim= dim)
+
+def kl_categorical_uniform(preds, num_features, eps=1e-16):
+    kl_div = preds * torch.log(preds + eps)
+    # if add_const:
+    #     const = np.log(num_edge_types)
+    #     kl_div += const
+    return -kl_div.sum() / (num_features * preds.size(0))  
+
+class GraphLearningLayer(nn.Module): 
+
+    def __init__(self, num_features, tau= 0.1):
+        super().__init__() 
+        self.num_features = num_features
+        self.tau = tau
+
+
+    def forward(self, feature_emb): 
+        # returns relationship between feature embeddings 
+        # transform the output by torch.nonzero into edge_index 
+        # relationship is computed as 
+        # 1) correlation 
+        # or 
+        # 2) sth else from the GRL book. 
+        # feature_emb: (num_features, node_emb_size)
+        logits = feature_emb@feature_emb.T # num_features, num_features
+        if self.training:
+            relation = gumbel_softmax(logits, self.tau, hard= False, dim= -1)
+            probs = torch.sigmoid(logits)
+            kl_loss = kl_categorical_uniform(probs, self.num_features)
+        else: 
+            relation = gumbel_softmax(logits, self.tau, hard= True, dim= -1)
+            kl_loss = None
+        relation_index = torch.nonzero(relation).T
+        relation_index = relation_index.to(relation.device)
+        return {
+            'relation': relation,
+            'relation_index': relation_index,
+            'kl_loss': kl_loss
+        }
+
+    def train_step(self, feature_emb):
+        # returns the relationship and the variational loss 
+        return feature_emb
+    
+    def test_step(self, feature_emb): 
+        # returns the relationship only.
+        # for the test step.
+        return feature_emb
